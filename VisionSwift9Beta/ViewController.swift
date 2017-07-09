b1c4f73d2cd145ef29ec6034f3b2f5b3a26cabc3
Victor Lee <myleevictor@gmail.com>
2017-07-09 14:15:39 +0800

    bring handler to background and UIView to main thread

--- VisionSwift9Beta/ViewController.swift
+++ VisionSwift9Beta/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 import Vision
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let image = UIImage(named: "sample3") else { return }
+        guard let image = UIImage(named: "sample5") else { return }
         let imageView = UIImageView(image: image)
         imageView.contentMode = .scaleAspectFit
         
@@ -25,60 +25,58 @@ class ViewController: UIViewController {
         
         view.addSubview(imageView)
         
-        // request
+        // Request
         let request = VNDetectFaceRectanglesRequest { (request, error) in
             if let error = error {
                 print("Failed to detect faces", error)
             }
-//            print(request)
-            
-            // results:
+            //            print(request)
+            // results:  Need to get back onto Main Thread after making handler async
             request.results?.forEach({ (result) in
-//                print(result)
+                //                print(result)
                 
-                // Cast results as VNFaceObservation and call its functions
-                
-                guard let vnFaceObservation = result as? VNFaceObservation else { return }
-                
-                // Use faceObservation's boundingBox values  to get x y origin coordinates to place faceView.  Remember origin starts from lower left.
-                let x = self.view.frame.width * vnFaceObservation.boundingBox.origin.x
-                let width = self.view.frame.width * vnFaceObservation.boundingBox.width
-                let height = scaledHeight * vnFaceObservation.boundingBox.height
-                
-                // 2 steps to getting correct y coordinate. First, 1 minus the origin.y. Second, subtract the height
-                
-                let y = scaledHeight * (1 - vnFaceObservation.boundingBox.origin.y) - height
-                
-                // Make box to place on face
-                let faceView = UIView()
-                faceView.backgroundColor = .yellow
-                faceView.alpha = 0.4
-                faceView.frame = CGRect(x: x, y: y, width: width, height: height)
-                self.view.addSubview(faceView)
-                
-                
-                
-                print(vnFaceObservation.boundingBox)
-                
+                DispatchQueue.main.async {
+                    // Cast results as VNFaceObservation and call its functions
+                    
+                    guard let vnFaceObservation = result as? VNFaceObservation else { return }
+                    
+                    // Use faceObservation's boundingBox values  to get x y origin coordinates to place faceView.  Remember origin starts from lower left.
+                    let x = self.view.frame.width * vnFaceObservation.boundingBox.origin.x
+                    let width = self.view.frame.width * vnFaceObservation.boundingBox.width
+                    let height = scaledHeight * vnFaceObservation.boundingBox.height
+                    
+                    // 2 steps to getting correct y coordinate. First, 1 minus the origin.y. Second, subtract the height
+                    
+                    let y = scaledHeight * (1 - vnFaceObservation.boundingBox.origin.y) - height
+                    
+                    // Make box to place on face
+                    let faceView = UIView()
+                    faceView.backgroundColor = .yellow
+                    faceView.alpha = 0.4
+                    faceView.frame = CGRect(x: x, y: y, width: width, height: height)
+                    self.view.addSubview(faceView)
+                    
+                    print(vnFaceObservation.boundingBox)
+                }
             })
-            
-            
         }
         // Handle the request
         guard let cgImage = image.cgImage else { return }
         // options return empty dictionary
-        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
         
-        do {
-            // Array of requests
-            try handler.perform([request])
+        // Image loading slow, so bring it to background thread
+        
+        DispatchQueue.global(qos: .background).async {
+            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
             
-        } catch let error {
-            print("Failed to perform request",error)
+            do {
+                // Array of requests
+                try handler.perform([request])
+                
+            } catch let error {
+                print("Failed to perform request",error)
+            }
         }
-        
     }
-    
-
 }
 
